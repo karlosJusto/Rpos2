dae9c3905e3997406971076eec9f6c65fcf55b8b
Carlos Justo <justo.karlos@gmail.com>
2024-12-27 18:20:30 +0100

    commit3

--- src/App.jsx
+++ src/App.jsx
@@ -34,7 +34,7 @@ function App() {
         <Route path="/scanner" element={<Scanner />} />
        
 
-        {/* Solo acceso admin */}
+        {/* Solo acceso administrador */}
 
         <Route path="/dashboard" element={<Dashboard />} />
 
